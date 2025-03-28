8a00f713b316c8451d5be25077a6b0afce147827
P1nque <toni.kopytov.99@mail.ru>
2025-03-28 16:27:42 +0800

    Create S11.cpp

---
+++ S11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int i = 0;
+    while (i<=100){
+        if (i % 3 == 0){
+            std::cout << i << std::endl;
+            }
+            i++;
+        }
+
+    return 0;
+}
