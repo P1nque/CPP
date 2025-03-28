bf7ba1e1d814ce32479eac6f3b5ffcf9b1c988f6
P1nque <toni.kopytov.99@mail.ru>
2025-03-28 16:11:25 +0800

    S01.cpp

---
+++ S01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int i = 10;
+    double d = 3.14;
+    float f = 2.71;
+    bool b = true;
+
+    std::cout << i + d << std::endl;
+    std::cout << d - f << std::endl;
+    std::cout << f * i << std::endl;
+    std::cout << d / i << std::endl;
+    return 0;
+}
