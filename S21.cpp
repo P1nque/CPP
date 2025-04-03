c0128da1f2319d059b4ef4df31390d94336c6551
P1nque <toni.kopytov.99@mail.ru>
2025-04-03 11:51:13 +0800

    Update S21.cpp

--- S21.cpp
+++ S21.cpp
@@ -2,14 +2,14 @@
 #include <fstream>
 using namespace std;
 
-int main(){
+int main() {
 
 setlocale(0, "Russian");
-int A,B;
-    std::cin >> A>>B;
-    std::ofstream out("output.txt");
-for (int i=A;i<=B;++i){
-    out << i*3<<std::endl;
+int A, B;
+    cin >> A >> B;
+    ofstream out("output.txt");
+for (int i = A ;i <= B; ++i) {
+    out << i * 3 << endl;
 }
 
 
