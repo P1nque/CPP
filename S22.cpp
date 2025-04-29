a74b1a1185b0b871f1f95303a157ade4f2faab8a
P1nque <toni.kopytov.99@mail.ru>
2025-04-29 16:41:14 +0800

    Update S22.cpp

--- S22.cpp
+++ S22.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
+using namespace std;
 
 int main() {
+    setlocale(0, "Russian");
     int i = 1;
-    std::ifstream f("output.txt");
-    std::string l, n;
-    while (std::getline(f, l)) {
-        std::stringstream s(l);
-        while (s >> n){
-            std::cout << i++ << ". " << n << std::endl;
-        }
+    int n;
+
+    ifstream outputFile("output.txt");
+
+    if (!outputFile.is_open()) {
+        cerr << "Файла 'output.txt' не существует" << endl;
+        return 1;
     }
+
+    while(outputFile >> n) {
+        cout << i << ". " << n << endl;
+        i++;
+    }
+    outputFile.close();
+    return 0;
 }
